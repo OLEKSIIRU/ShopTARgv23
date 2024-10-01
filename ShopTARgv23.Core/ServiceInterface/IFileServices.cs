6625a2e5f6915e93381240d5608109118619d7e2
oleksiirudenkox@gmail.com <oleksiirudenkox@gmail.com>
2024-10-01 12:28:50 +0300

    Add project files.

---
+++ ShopTARgv23.Core/ServiceInterface/IFileServices.cs
@@ -0,0 +1,14 @@
+﻿using ShopTARgv23.Core.Domain;
+using ShopTARgv23.Core.Dto;
+
+namespace ShopTARgv23.Core.ServiceInterface
+{
+    public interface IFileServices
+    {
+        void FilesToApi(SpaceshipDto dto, Spaceship spaceship);
+
+        Task<FileToApi> RemoveImageFromApi(FileToApiDto dto);
+
+        Task<List<FileToApi>> RemoveImageFromApi(FileToApiDto[] dtos);
+    }
+}
